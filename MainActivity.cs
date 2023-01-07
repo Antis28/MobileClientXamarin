fad185a78f80ebafd9b3bcf875c37822775f5113
Antis <Antis28@mail.ru>
2023-01-07 20:40:06 +0300

    add Hello world с выходом из приложения

--- MainActivity.cs
+++ MainActivity.cs
@@ -2,6 +2,8 @@
 using Android.OS;
 using Android.Runtime;
 using AndroidX.AppCompat.App;
+using Android.Widget;
+using System;
 
 namespace MobileClientXamarin
 {
@@ -11,15 +13,36 @@ namespace MobileClientXamarin
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-        }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            CreateButtonHandler();
+        }        
 
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {           
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        private void CreateButtonHandler()
+        {
+            var bntOk = FindViewById<Button>(Resource.Id.button1);
+            bntOk.Click += BntOk_Click;
+        }
+
+        private void BntOk_Click(object sender, EventArgs e)
+        {
+
+            var alert = new AndroidX.AppCompat.App.AlertDialog.Builder(this);
+            alert.SetTitle("Внимание");
+            alert.SetMessage("Вы действительно хотите выйти из приложения?");
+            alert.SetCancelable(false);
+            alert.SetPositiveButton("Да", delegate { Java.Lang.JavaSystem.Exit(0); });
+            alert.SetNegativeButton("Нет", delegate { });
+
+            RunOnUiThread(() =>
+            {
+                alert.Show();
+            }
+            );
         }
     }
 }
