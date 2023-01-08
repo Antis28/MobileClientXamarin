47d1083fd557378187a601b7c4a9e555fe79c194
Antis <Antis28@mail.ru>
2023-01-08 17:58:21 +0300

    подбор цвета

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using Android.App;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Application(Icon = "@mipmap/ic_launcher")]
+[assembly: Application(Icon = "@mipmap/ic_launcher", Theme = "@style/AppTheme")]
 
 // Version information for an assembly consists of the following four values:
 //
